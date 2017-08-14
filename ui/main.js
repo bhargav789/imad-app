a79b4b36919c80512c3957e2e4aea2c4d0317953
bhargav789 <30655078+bhargav789@users.noreply.github.com>
2017-08-14 18:59:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 alert ('Are you valid user');
-var img = document.getelementById('img');
+var img = document.getelementById('madi');
 img.onclick = function() {
     img.style.marginleft='100px';
 };
